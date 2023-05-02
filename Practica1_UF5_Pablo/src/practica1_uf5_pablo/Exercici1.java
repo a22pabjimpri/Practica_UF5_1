8b0578277ab05163ee3e2d5bd6507b6a02a216f6
ausias <ausias@hINFO6-5.xiam.cat>
2023-05-02 12:44:16 +0200

    Ejercicio 1111111111111111111111111111111

---
+++ Practica1_UF5_Pablo/src/practica1_uf5_pablo/Exercici1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1_uf5_pablo;
+
+/**
+ *
+ * @author ausias
+ */
+public class Exercici1 {
+    public static int divisio(int numa, int numb)throws ArithmeticException{
+        return numa/numb;
+    }
+    
+}
